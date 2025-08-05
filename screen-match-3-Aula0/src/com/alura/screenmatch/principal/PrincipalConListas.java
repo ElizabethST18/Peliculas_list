10ba33180a71685a1ec5752b1f899b9c54764a8f
Elizabeth Sanchez <elizabethsanchez1819@gmail.com>
2025-08-05 00:00:38 -0300

    creacion de lista con series y peliculas
    creo una carpeta principal con la clase principal y principal con listas, que tiene las películas y series en una lista de títulos.

---
+++ screen-match-3-Aula0/src/com/alura/screenmatch/principal/PrincipalConListas.java
@@ -0,0 +1,24 @@
+package com.alura.screenmatch.principal;
+
+import com.alura.screenmatch.modelos.Pelicula;
+import com.alura.screenmatch.modelos.Serie;
+import com.alura.screenmatch.modelos.Titulo;
+
+import java.util.ArrayList;
+
+public class PrincipalConListas {
+    public static void main(String[] args) {
+
+        Pelicula miPelicula = new Pelicula("Encanto", 2021);
+        Pelicula otraPelicula = new Pelicula("Avatar" , 2023);
+        var peliculaDeBruno = new Pelicula("el señor de los anillos", 2001);
+        Serie lost = new Serie("Lost" , 2000);
+
+        ArrayList<Titulo> lista = new ArrayList<>();
+        lista.add(peliculaDeBruno);
+        lista.add(miPelicula);
+        lista.add(otraPelicula);
+        lista.add(lost);
+        
+    }
+}
